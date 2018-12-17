e0042a1ad68f3f50c3180532415aa541d0b52d4d
Macbook-hmroh <nhm0721@gmail.com>
2018-12-18 01:44:23 +0900

    Edit /android files to apply react-native-navigation
    wix의 RNN Document를 참고해서 안드로이드 관련 설정 변경;
    Android app 실행 시, "$ react-native run-android"가 아니라 "$ npm run android"로 변경;

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ReactNative04
+
+build Android application, using:
+
+```bash
+$ npm run android
+```
+
+Ref: https://wix.github.io/react-native-navigation/#/docs/Installing?id=_71-build-app-with-gradle-command
--- weatherApp/android/app/src/main/java/com/weatherapp/MainActivity.java
+++ weatherApp/android/app/src/main/java/com/weatherapp/MainActivity.java
@@ -1,15 +1,22 @@
 package com.weatherapp;
 
-import com.facebook.react.ReactActivity;
+// import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+// public class MainActivity extends ReactActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "weatherApp";
-    }
+//     /**
+//      * Returns the name of the main component registered from JavaScript.
+//      * This is used to schedule rendering of the component.
+//      */
+//     @Override
+//     protected String getMainComponentName() {
+//         return "weatherApp";
+//     }
+// }
+
+/**
+ * https://wix.github.io/react-native-navigation/#/docs/Installing?id=android
+ */
+import com.reactnativenavigation.NavigationActivity;
+public class MainActivity extends NavigationActivity {
 }
